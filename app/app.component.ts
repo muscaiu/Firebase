9f1ceaa0ff5da2ddd056f3f57ac08364b2e2862a
Cristian Muscalu <muscaiu@yahoo.com>
2016-06-20 12:59:32 +0300

    added a firebase list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,13 @@
 import {Component} from '@angular/core';
+import {FirebaseListComponent} from './firebase-list.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>hi<h1>'
+    template: `
+    <h2>main app;</h2>
+    <firebase-list></firebase-list>
+    `,
+    directives: [FirebaseListComponent]
 })
 
 export class AppComponent{
---
+++ app/firebase-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'firebase-list',
+    template:`
+        <h2>list from firebase</h2>
+    `
+})
+
+export class FirebaseListComponent{
+
+}
